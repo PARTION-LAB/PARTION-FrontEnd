2147b622376a506cae01890d645e5b60bcc33a27
wildyoung <ondskan@naver.com>
2026-06-09 09:46:13 +0900

    feat: add router navigation and auth tabs

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,21 @@
 <script setup>
-import { computed, ref } from 'vue'
+import { computed } from 'vue'
+import { RouterView, useRoute, useRouter } from 'vue-router'
 import AppHeader from './components/layout/AppHeader.vue'
-import AuthView from './views/AuthView.vue'
-import BoardView from './views/BoardView.vue'
-import HomeView from './views/HomeView.vue'
-import InvestView from './views/InvestView.vue'
-import LedgerView from './views/LedgerView.vue'
-import MarketView from './views/MarketView.vue'
-import RegisterProductView from './views/RegisterProductView.vue'
 
-const activeView = ref('products')
+const route = useRoute()
+const router = useRouter()
 
-const currentView = computed(() => {
-  if (activeView.value === 'register') {
-    return RegisterProductView
-  }
-
-  if (activeView.value === 'invest') {
-    return InvestView
-  }
-
-  if (activeView.value === 'market') {
-    return MarketView
-  }
-
-  if (activeView.value === 'board') {
-    return BoardView
-  }
-
-  if (activeView.value === 'ledger') {
-    return LedgerView
-  }
-
-  if (activeView.value === 'auth') {
-    return AuthView
-  }
-
-  return HomeView
-})
+const activeView = computed(() => route.meta.navKey || 'products')
 
 function navigate(view) {
-  activeView.value = view
-  window.scrollTo({ top: 0, behavior: 'smooth' })
+  router.push({ name: view })
 }
 </script>
 
 <template>
   <AppHeader :active-view="activeView" @navigate="navigate" />
-  <component :is="currentView" @navigate="navigate" />
+  <RouterView v-slot="{ Component }">
+    <component :is="Component" @navigate="navigate" />
+  </RouterView>
 </template>
---
+++ src/router/index.js
@@ -0,0 +1,67 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import AuthView from '../views/AuthView.vue'
+import BoardView from '../views/BoardView.vue'
+import HomeView from '../views/HomeView.vue'
+import InvestView from '../views/InvestView.vue'
+import LedgerView from '../views/LedgerView.vue'
+import MarketView from '../views/MarketView.vue'
+import RegisterProductView from '../views/RegisterProductView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'products',
+    component: HomeView,
+    meta: { navKey: 'products' },
+  },
+  {
+    path: '/invest',
+    name: 'invest',
+    component: InvestView,
+    meta: { navKey: 'invest' },
+  },
+  {
+    path: '/market',
+    name: 'market',
+    component: MarketView,
+    meta: { navKey: 'market' },
+  },
+  {
+    path: '/board',
+    name: 'board',
+    component: BoardView,
+    meta: { navKey: 'board' },
+  },
+  {
+    path: '/ledger',
+    name: 'ledger',
+    component: LedgerView,
+    meta: { navKey: 'ledger' },
+  },
+  {
+    path: '/auth',
+    name: 'auth',
+    component: AuthView,
+    meta: { navKey: 'auth' },
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterProductView,
+    meta: { navKey: 'register' },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/',
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  scrollBehavior() {
+    return { top: 0 }
+  },
+})
+
+export default router
