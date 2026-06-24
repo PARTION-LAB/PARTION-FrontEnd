a9d8c07c1ffcfe4dbf129507812dcd2b1967dc63
송현민 <ondskan@naver.com>
2026-06-24 14:37:06 +0900

    Add Datadog RUM instrumentation

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
+import { addVueError } from '@datadog/browser-rum-vue'
 import './assets/style.css'
 import App from './App.vue'
+import { initDatadogRum } from './observability/datadogRum'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+initDatadogRum()
+
+const app = createApp(App)
+
+app.config.errorHandler = addVueError
+app.use(router).mount('#app')
